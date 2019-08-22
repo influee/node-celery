6090bef0f400d085593f0106ba86562a44dd937a
nejcsm <nejc.smolej@gmail.com>
2019-08-22 12:07:40 +0200

    Priority option added

--- protocol.js
+++ protocol.js
@@ -1,42 +1,56 @@
 var uuid = require('uuid');
 
-var fields = ['task', 'id', 'args', 'kwargs', 'retries', 'eta', 'expires', 'queue',
-              'taskset', 'chord', 'utc', 'callbacks', 'errbacks', 'timeouts'];
-
+var fields = [
+  'task',
+  'id',
+  'args',
+  'kwargs',
+  'retries',
+  'eta',
+  'expires',
+  'queue',
+  'taskset',
+  'chord',
+  'utc',
+  'callbacks',
+  'errbacks',
+  'timeouts',
+  'priority',
+];
 
 function formatDate(date) {
-    return new Date(date).toISOString();
+  return new Date(date).toISOString();
 }
 
 function createMessage(task, args, kwargs, options, id) {
-    args = args || [];
-    kwargs = kwargs || {};
+  args = args || [];
+  kwargs = kwargs || {};
 
-    var message = {
-        task: task,
-        args: args,
-        kwargs: kwargs
-    };
+  var message = {
+    task: task,
+    args: args,
+    kwargs: kwargs,
+  };
 
-    message.id = id || uuid.v4();
-    for (var o in options) {
-        if (options.hasOwnProperty(o)) {
-            if (fields.indexOf(o) === -1) {
-                throw "invalid option: " + o;
-            }
-            message[o] = options[o];
-        }
+  message.id = id || uuid.v4();
+  for (var o in options) {
+    if (options.hasOwnProperty(o)) {
+      if (fields.indexOf(o) === -1) {
+        throw 'invalid option: ' + o;
+      }
+      message[o] = options[o];
     }
+  }
 
-    if (message.eta) {
-        message.eta = formatDate(message.eta);
-    }
+  if (message.eta) {
+    message.eta = formatDate(message.eta);
+  }
 
-    if (message.expires) {
-        message.expires = formatDate(message.expires);
-    }
+  if (message.expires) {
+    message.expires = formatDate(message.expires);
+  }
 
-    return JSON.stringify(message);
+  return JSON.stringify(message);
 }
 
 exports.createMessage = createMessage;
