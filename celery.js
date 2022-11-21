0e2d095a0b9a65445ec796d468ed172d9051496d
nejcsm <nejc.smolej@gmail.com>
2022-11-21 15:00:29 +0100

    Bug fix when setting protocol

--- celery.js
+++ celery.js
@@ -11,9 +11,9 @@ var debug = process.env.NODE_CELERY_DEBUG === '1' ? console.info : function() {}
 
 var supportedProtocols = ['amqp', 'amqps', 'redis'];
 function getProtocol(kind, options) {
-  const protocol = url.parse(options.url).protocol.slice(0, -1);
-  if (protocol === 'amqps') {
-    protocol = 'amqp';
+  var protocol = url.parse(options.url).protocol.slice(0, -1);
+  if (protocol === "amqps") {
+    protocol = "amqp";
   }
   if (supportedProtocols.indexOf(protocol) === -1) {
     throw new Error(util.format('Unsupported %s type: %s', kind, protocol));
